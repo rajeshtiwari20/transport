2fc03f4203a26f5e48c7d82f251f71085df7686b
RAJESH TIWARI <51081565+rajeshtiwari20@users.noreply.github.com>
2026-06-17 10:32:09 +0530

    allowed address provided

--- src/main/java/com/jaijobner/transport_new/config/SecurityConfig.java
+++ src/main/java/com/jaijobner/transport_new/config/SecurityConfig.java
@@ -81,7 +81,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://transport.jaijobner.com/",
+                "http://transport.jaijobner.com/",
+                "https://transport.jaijobner.com/latest",
+                "http://transport.jaijobner.com/latest"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
